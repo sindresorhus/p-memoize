19b418ec6e3796db3dc0d29c819517c29b9b2b02
Richie Bendall <richiebendall@gmail.com>
2021-10-13 22:24:06 +1300

    Allow cache to be async (#26)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- readme.md
+++ readme.md
@@ -8,7 +8,7 @@ Useful for speeding up consecutive function calls by caching the result of calls
 
 By default, **only the memoized function's first argument is considered** via strict equality comparison. If you need to cache multiple arguments or cache `object`s *by value*, have a look at alternative [caching strategies](#caching-strategy) below.
 
-This package is similar to [mem](https://github.com/sindresorhus/mem) but with async-specific enhancements; in particular, it does not cache rejected promises by default (unless the [`cachePromiseRejection`](#cachePromiseRejection) option is set).
+This package is similar to [mem](https://github.com/sindresorhus/mem) but with async-specific enhancements; in particular, it allows for asynchronous caches and does not cache rejected promises by default (unless the [`cachePromiseRejection`](#cachePromiseRejection) option is set).
 
 ## Install
 
@@ -19,25 +19,30 @@ $ npm install p-memoize
 ## Usage
 
 ```js
+import {setTimeout as delay} from 'node:timer/promises';
 import pMemoize from 'p-memoize';
 import got from 'got';
 
-const memoizedGot = pMemoize(got, {maxAge: 1000});
+const memoizedGot = pMemoize(got);
 
-memoizedGot('https://sindresorhus.com');
+await memoizedGot('https://sindresorhus.com');
 
 // This call is cached
-memoizedGot('https://sindresorhus.com');
+await memoizedGot('https://sindresorhus.com');
 
-setTimeout(() => {
-	// This call is not cached as the cache has expired
-	memoizedGot('https://sindresorhus.com');
-}, 2000);
+await delay(2000);
+
+// This call is not cached as the cache has expired
+await memoizedGot('https://sindresorhus.com');
 ```
 
 ### Caching strategy
 
-See the [Caching strategy for `mem`](https://github.com/sindresorhus/mem#options).
+Similar to the [caching strategy for `mem`](https://github.com/sindresorhus/mem#options) with the following exceptions:
+
+- Promises returned from a memoized function will be cached internally and take priority over `cache`. The promise cache does not persist outside of the current instance and properties assigned to a returned promise will not be kept. All cached promises can be cleared with [`pMemoizeClear()`](#pmemoizeclearfn).
+- `.get()` and `.has()` methods on `cache` can return a promise instead of returning a value immediately.
+- Instead of `.set()` being provided an object with the properties `value` and `maxAge`, it will only be provided `value` as the first argument. If you want to implement time-based expiry, consider [doing so in `cache`](#time-based-cache-expiration).
 
 ## API
 
@@ -64,13 +69,6 @@ Default: `false`
 
 Cache rejected promises.
 
-##### maxAge
-
-Type: `number`\
-Default: `Infinity`
-
-The milliseconds until the cache expires.
-
 ##### cacheKey
 
 Type: `Function`\
@@ -81,7 +79,7 @@ Determines the cache key for storing the result based on the function arguments.
 
 A `cacheKey` function can return any type supported by `Map` (or whatever structure you use in the `cache` option).
 
-See the [caching strategy](https://github.com/sindresorhus/mem#caching-strategy) section in the `mem` package for more information.
+See the [caching strategy](#caching-strategy) section for more information.
 
 ##### cache
 
@@ -92,11 +90,63 @@ Use a different cache storage. Must implement the following methods: `.has(key)`
 
 See the [caching strategy](https://github.com/sindresorhus/mem#caching-strategy) section in the `mem` package for more information.
 
+### pMemoizeDecorator(options)
+
+Returns a [decorator](https://github.com/tc39/proposal-decorators) to memoize class methods or static class methods.
+
+Notes:
+
+- Only class methods and getters/setters can be memoized, not regular functions (they aren't part of the proposal);
+- Only [TypeScript’s decorators](https://www.typescriptlang.org/docs/handbook/decorators.html#parameter-decorators) are supported, not [Babel’s](https://babeljs.io/docs/en/babel-plugin-proposal-decorators), which use a different version of the proposal;
+- Being an experimental feature, they need to be enabled with `--experimentalDecorators`; follow TypeScript’s docs.
+
+#### options
+
+Type: `object`
+
+Same as options for `pMemoize()`.
+
+```ts
+import {pMemoizeDecorator} from 'p-memoize';
+
+class Example {
+	index = 0
+
+	@pMemoizeDecorator()
+	async counter() {
+		return ++this.index;
+	}
+}
+
+class ExampleWithOptions {
+	index = 0
+
+	@pMemoizeDecorator()
+	async counter() {
+		return ++this.index;
+	}
+}
+```
+
 ### pMemoizeClear(memoized)
 
 Clear all cached data of a memoized function.
 
 It will throw when given a non-memoized function.
+
+## Tips
+
+### Time-based cache expiration
+
+```js
+import pMemoize from 'p-memoize';
+import ExpiryMap from 'expiry-map';
+import got from 'got';
+
+const cache = new ExpiryMap(10000); // Cached values expire after 10 seconds
+
+const memoizedGot = pMemoize(got, {cache});
+```
 
 ## Related
 
---
+++ test.ts
@@ -0,0 +1,203 @@
+import test from 'ava';
+import serializeJavascript from 'serialize-javascript';
+import pMemoize, {pMemoizeDecorator, pMemoizeClear} from './index.js';
+
+test('memoize', async t => {
+	let index = 0;
+	const fixture = async () => index++;
+	const memoized = pMemoize(fixture);
+	t.is(await memoized(), 0);
+	t.is(await memoized(), 0);
+	t.is(await memoized(), 0);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(undefined), 0);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(undefined), 0);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized('foo'), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized('foo'), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized('foo'), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized('foo', 'bar'), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized('foo', 'bar'), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized('foo', 'bar'), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(1), 2);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(1), 2);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(null), 3);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(null), 3);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(fixture), 4);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(fixture), 4);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(true), 5);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(true), 5);
+
+	// Ensure that functions are stored by reference and not by "value" (e.g. their `.toString()` representation)
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(() => index++), 6);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(() => index++), 7);
+});
+
+test('cacheKey option', async t => {
+	let index = 0;
+	const fixture = async (..._arguments: any) => index++;
+	const memoized = pMemoize(fixture, {cacheKey: ([firstArgument]) => String(firstArgument)});
+	t.is(await memoized(1), 0);
+	t.is(await memoized(1), 0);
+	t.is(await memoized('1'), 0);
+	t.is(await memoized('2'), 1);
+	t.is(await memoized(2), 1);
+});
+
+test('memoize with multiple non-primitive arguments', async t => {
+	let index = 0;
+	const memoized = pMemoize(async () => index++, {cacheKey: JSON.stringify});
+	t.is(await memoized(), 0);
+	t.is(await memoized(), 0);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized({foo: true}, {bar: false}), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized({foo: true}, {bar: false}), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized({foo: true}, {bar: false}, {baz: true}), 2);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized({foo: true}, {bar: false}, {baz: true}), 2);
+});
+
+test('memoize with regexp arguments', async t => {
+	let index = 0;
+	const memoized = pMemoize(async () => index++, {cacheKey: serializeJavascript});
+	t.is(await memoized(), 0);
+	t.is(await memoized(), 0);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(/Sindre Sorhus/), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(/Sindre Sorhus/), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(/Elvin Peng/), 2);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(/Elvin Peng/), 2);
+});
+
+test('memoize with Symbol arguments', async t => {
+	let index = 0;
+	const argument1 = Symbol('fixture1');
+	const argument2 = Symbol('fixture2');
+	const memoized = pMemoize(async () => index++);
+	t.is(await memoized(), 0);
+	t.is(await memoized(), 0);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(argument1), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(argument1), 1);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(argument2), 2);
+	// @ts-expect-error Argument type does not match
+	t.is(await memoized(argument2), 2);
+});
+
+test('cache option', async t => {
+	let index = 0;
+	const fixture = async (..._arguments: any) => index++;
+	const memoized = pMemoize(fixture, {
+		cache: new WeakMap(),
+		cacheKey: <ReturnValue>([firstArgument]: [ReturnValue]): ReturnValue => firstArgument,
+	});
+	const foo = {};
+	const bar = {};
+	t.is(await memoized(foo), 0);
+	t.is(await memoized(foo), 0);
+	t.is(await memoized(bar), 1);
+	t.is(await memoized(bar), 1);
+});
+
+test('preserves the original function name', t => {
+	t.is(pMemoize(async function foo() {}).name, 'foo'); // eslint-disable-line func-names, @typescript-eslint/no-empty-function
+});
+
+test('.pMemoizeClear()', async t => {
+	let index = 0;
+	const fixture = async () => index++;
+	const memoized = pMemoize(fixture);
+	t.is(await memoized(), 0);
+	t.is(await memoized(), 0);
+	pMemoizeClear(memoized);
+	t.is(await memoized(), 1);
+	t.is(await memoized(), 1);
+});
+
+test('prototype support', async t => {
+	class Unicorn {
+		index = 0;
+		async foo() {
+			return this.index++;
+		}
+	}
+
+	Unicorn.prototype.foo = pMemoize(Unicorn.prototype.foo);
+
+	const unicorn = new Unicorn();
+
+	t.is(await unicorn.foo(), 0);
+	t.is(await unicorn.foo(), 0);
+	t.is(await unicorn.foo(), 0);
+});
+
+test('.pMemoizeDecorator()', async t => {
+	let returnValue = 1;
+	const returnValue2 = 101;
+
+	class TestClass {
+		@pMemoizeDecorator()
+		async counter() {
+			return returnValue++;
+		}
+
+		@pMemoizeDecorator()
+		async counter2() {
+			return returnValue2;
+		}
+	}
+
+	const alpha = new TestClass();
+	t.is(await alpha.counter(), 1);
+	t.is(await alpha.counter(), 1, 'The method should be memoized');
+	t.is(await alpha.counter2(), 101, 'The method should be memoized separately from the other one');
+
+	const beta = new TestClass();
+	t.is(await beta.counter(), 2, 'The method should not be memoized across instances');
+});
+
+test('memClear() throws when called with a plain function', t => {
+	t.throws(() => {
+		pMemoizeClear(async () => {}); // eslint-disable-line @typescript-eslint/no-empty-function
+	}, {
+		message: 'Can\'t clear a function that was not memoized!',
+		instanceOf: TypeError,
+	});
+});
+
+test('memClear() throws when called on an unclearable cache', t => {
+	const fixture = async () => 1;
+	const memoized = pMemoize(fixture, {
+		cache: new WeakMap(),
+	});
+
+	t.throws(() => {
+		pMemoizeClear(memoized);
+	}, {
+		message: 'The cache Map can\'t be cleared!',
+		instanceOf: TypeError,
+	});
+});
